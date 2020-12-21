a2087794cdc6ee48704533b3219da78a3b8348a8
Stacy Hung <sshung@gmail.com>
2020-12-21 13:56:38 -0800

    add basic pages to repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+import React from "react";
+import { Link, Switch, Route } from "react-router-dom";
+import logo from './stripe-s-brands.svg';
 import './App.css';
+import Blog from "./pages/Blog"
+
 
 function App() {
   return (
     <div className="App">
+      <nav>
+        <Link to="/">
+          <div class="logo">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h3>Be Curious</h3>
+          </div>
+        </Link>
+        <div class="links">
+          <Link to="/blog">Blog</Link>
+          <Link to="/recipes">Recipes</Link>
+          <Link to="/travelling">Travelling</Link>
+          <Link to="/projects">Projects</Link>
+          <Link to="/about-me">About Me</Link>
+        </div>
+      </nav>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Stacy's Space</h1>
       </header>
+
+      <body>
+        <Switch>
+          <Route exact path="/">
+            <h1>Home</h1>
+          </Route>
+          <Route path="/blog">
+            <h1>Stacy's Blog</h1>
+          </Route>
+          <Route path="/recipes">
+            <h1>Recipes</h1>
+          </Route>
+          <Route path="/travelling">
+            <h1>Stacy's Travelling Adventures</h1>
+          </Route>
+          <Route path="/projects">
+            <h1>Stacy's Projects</h1>
+          </Route>
+          <Route path="/about-me">
+            <h1>Hi, I'm Stacy!</h1>
+          </Route>
+
+        </Switch>
+      </body>
+
     </div>
+
   );
 }
 
---
+++ src/pages/Blog.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Blog() {
+
+
+
+    return (
+        <Blog>
+            <h1>Stacy's Blog</h1>
+        </Blog>
+    )
+}
+
+export default Blog;
