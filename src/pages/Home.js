7bda53e00b775b6cc959e6f42ea56a9e589b1402
Stacy Hung <sshung@gmail.com>
2020-12-30 14:49:17 -0800

    fix intro section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,16 @@
 import React from "react";
 import profilePic from '../IMG_7200-EFFECTS.jpg';
+// import profilePic from '../Iceland-Stacy-golden-circle2.jpg';
 import '../css/Home.css'
 
 function Home() {
 
     return (
-        <div className="intro">
-            <h1 className="section-title">Hi, I am <strong>Stacy Hung</strong></h1>
-            <h3 className="section-subtitle">Aspiring Front-end Developer</h3>
+        <section className="intro">
+            <h1 className="section-title">Hi, I am <br /><strong>Stacy Hung</strong></h1>
+            <h3 className="section-subtitle">Front-end Dev</h3>
             <img className="intro-img" src={profilePic} alt="me looking out at NZ" id="profile-pic" />
-        </div>
+        </section>
 
     )
 }
