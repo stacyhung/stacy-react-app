c7f768e47783afe7bc248e31934aaad1ecbe5718
Stacy Hung <sshung@gmail.com>
2021-01-06 21:33:22 -0800

    update intro

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,7 +16,7 @@ function Home() {
             </section>
             <p className="intro-content">I'm a bioinformatics scientist who recently discovered an untapped
             passion for web design and development. When I'm not coding, I can be found cycling on the North Shore,
-            sipping on a matcha / tumeric latte, or coming up with an itinerary for my next big trip.
+            sipping on a matcha latte, or coming up with an itinerary for my next big trip.
             Here's my outlet - enjoy!</p>
         </>
     )
