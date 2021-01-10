9ed7568653bab7225baeea2b023bdd9ae9951aa2
Stacy Hung <sshung@gmail.com>
2021-01-10 13:12:10 -0800

    add data for rest of trip photos

--- src/data/tripData.js
+++ src/data/tripData.js
@@ -21,22 +21,56 @@ export default [
     {
         id: "japan",
         name: "Japan",
-        desc: "Two weeks by shinkasen from Tokyo to Hiroshima and back",
+        desc: "Two weeks by Shinkansen from Tokyo to Hiroshima and back",
         img_cover: "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/japan/IMG_1391.jpg",
-        img_alt: "Temple garden in Kyoto, Japan"
+        img_alt: "Temple garden in Kyoto, Japan",
+        img_src: [
+            "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/japan/IMG_0978.jpg",
+            "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/japan/IMG_1015.jpg",
+            "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/japan/IMG_1391.jpg",
+            "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/japan/IMG_1569.jpg",
+            "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/japan/IMG_4961.jpg",
+            "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/japan/IMG_4975.jpg",
+            "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/japan/IMG_5032.jpg",
+            "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/japan/IMG_5072.jpg",
+            "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/japan/IMG_5102-COLLAGE.jpg",
+            "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/japan/IMG_5145.jpg"
+        ]
     },
     {
         id: "iceland",
         name: "Iceland",
         desc: "Two weeks driving the Golden Circle and Ring Road",
         img_cover: "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/iceland/2-6-Vestmannaeyjar-puffins.small.jpg",
-        img_alt: "puffins in Iceland"
+        img_alt: "puffins in Iceland",
+        img_src: [
+            "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/iceland/1-Pingvellir-male_and_female_goats.jpg",
+            "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/iceland/2-1-Geysir-geyser_exploding.jpg",
+            "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/iceland/2-11-Vestmannaeyjar-puffin_closeup.jpg",
+            "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/iceland/2-19-Vestmannaeyjar-Dennis_on_Elfjell.jpg",
+            "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/iceland/2-3IMG_3115.jpg",
+            "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/iceland/3-11-Jorlokson_glaciers.jpg",
+            "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/iceland/3-6-Skaftafell-waterfall.jpg",
+            "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/iceland/4-5-dettifoss.jpg",
+            "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/iceland/4-8-driving_Mvytan.jpg",
+            "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/iceland/6-2-horsebackriding.jpg"
+        ]
     },
     {
         id: "yukon",
         name: "The Yukon",
         desc: "Three days snow-biking, dog-sledding and watching the Northen Lights",
         img_cover: "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/yukon/4-IMG_0209.jpg",
-        img_alt: "dog-sledding in the Yukon"
+        img_alt: "dog-sledding in the Yukon",
+        img_src: [
+            "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/yukon/1-IMG_0168.jpg",
+            "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/yukon/2-IMG_4121.jpg",
+            "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/yukon/3-IMG_4205.jpg",
+            "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/yukon/4-IMG_0209.jpg",
+            "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/yukon/5-IMG_4145.jpg",
+            "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/yukon/6-IMG_4170.jpg",
+            "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/yukon/7-IMG_4201.jpg",
+            "https://raw.githubusercontent.com/stacyhung/stacy-react-app/master/images/trips/yukon/8-IMG_4091.jpg"
+        ]
     }
 ]
