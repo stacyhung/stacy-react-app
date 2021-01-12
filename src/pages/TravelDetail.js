7d17cd8b12d5d2e73184a5b25e314a4015582d63
Stacy Hung <sshung@gmail.com>
2021-01-12 09:30:21 -0800

    fix carousel width

--- src/pages/TravelDetail.js
+++ src/pages/TravelDetail.js
@@ -5,25 +5,25 @@ import tripData from "../data/tripData";
 import "../css/trip.css";
 
 function TravelDetail() {
+  const { tripId } = useParams();
 
-    const { tripId } = useParams();
+  // get the trip from the id
+  const thisTrip = tripData.find((trip) => trip.id === tripId);
 
-    // get the trip from the id
-    const thisTrip = tripData.find(trip => trip.id === tripId);
-
-    // use Bootstrap Carousel Component
-    return (
-        <Carousel>
-            {thisTrip.img_src.map((img, index) => (
-                <Carousel.Item interval={1500}>
-                    <img 
-                        className="carousel-img d-block w-100" 
-                        src={img}
-                        alt={`NZ-${index}`}/>
-                </Carousel.Item>
-        ))}
-        </Carousel>
-    )
+  // use Bootstrap Carousel Component
+  return (
+    <Carousel className="carousel">
+      {thisTrip.img_src.map((img, index) => (
+        <Carousel.Item interval={1500}>
+          <img
+            className="carousel-img d-block w-100"
+            src={img}
+            alt={`NZ-${index}`}
+          />
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
 }
 
 export default TravelDetail;
